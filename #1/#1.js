1d7ece24466277fe3a13fbb909d8f115c48c17c3
Roberto Nantes <eternal.becoming@gmail.com>
2017-08-16 21:44:37 -0300

    Fix alignment

--- #1/#1.js
+++ #1/#1.js
@@ -1,5 +1,5 @@
-var userName = prompt("What's your name?");
-var userAge  = prompt("How old are you?");
+var userName   = prompt("What's your name?");
+var userAge    = prompt("How old are you?");
 var userReddit = prompt("What's your reddit username?");
 
 alert( "Your name is " + userName + ", you are " + userAge + " years old, and your username is " + userReddit );
